dd9ebcf94ffda96aba2b56c38055cd77dd098760
yangisee <yvincent74@gmail.com>
2016-11-12 17:13:26 -0500

    add list entries

--- app/src/main/java/group88/cookhelper/MainActivity.java
+++ app/src/main/java/group88/cookhelper/MainActivity.java
@@ -2,12 +2,20 @@ package group88.cookhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView mListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView) findViewById(R.id.recipe_list_view);
+        String[] listItems ={"Spaghetti","Steak","Pho","Grilled Chicken","Beef Stew","Meat Pizza","Ice Cream"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        mListView.setAdapter(adapter);
+
     }
 }
