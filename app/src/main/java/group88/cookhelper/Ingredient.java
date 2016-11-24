43adfe71dffb394f1f9c633d43d8f8f299f7d6ec
yangisee <yvincent74@gmail.com>
2016-11-24 16:14:36 -0500

    Empty java file created

---
+++ app/src/main/java/group88/cookhelper/Ingredient.java
@@ -0,0 +1,11 @@
+package group88.cookhelper;
+
+/**
+ * Created by YANG on 2016-11-24.
+ */
+
+public class Ingredient {
+/**
+ * implement ingredient here
+ */
+}
---
+++ app/src/main/java/group88/cookhelper/Recipe.java
@@ -0,0 +1,12 @@
+package group88.cookhelper;
+
+/**
+ * Created by YANG on 2016-11-24.
+ */
+
+public class Recipe {
+
+/** write recipe class here
+ * private variable with getter and setter
+ */
+}
